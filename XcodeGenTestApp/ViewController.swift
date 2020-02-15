35b1671653f0c461912fe7611845a711b5c86eee
yama07 <n.116@icloud.com>
2020-02-15 14:49:14 +0900

    Initial Commit

---
+++ XcodeGenTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGenTestApp
+//
+//  Created by Yamamoto on 2020/02/15.
+//  Copyright © 2020 Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
